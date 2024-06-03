3769572444a5c67850427800938950085b5979d9
Jasper-Finn Schröder <jasper-finn.schroeder@deutschebahn.com>
2024-06-03 14:37:05 +0200

    feature: added List and Button to tags_screen.dart and changed the backgroundcolor as well as text color for all screens

--- lib/screens/tags_screen.dart
+++ lib/screens/tags_screen.dart
@@ -1,7 +1,15 @@
+import 'package:finance_tracker/screens/add_tag_screen.dart';
 import 'package:flutter/material.dart';
 
-class TagsScreen extends StatelessWidget {
+class TagsScreen extends StatefulWidget {
   const TagsScreen({super.key});
+
+  @override
+  _TagScreenState createState() => _TagScreenState();
+}
+
+class _TagScreenState extends State<TagsScreen> {
+  final List tags = ['Groceries', 'Utilities', 'Entertainment'];
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +17,35 @@ class TagsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Tags'),
       ),
-      body: const Center(
-        child: Text('Tags Screen'),
+      body: ListView.builder(
+        itemCount: tags.length,
+        itemBuilder: (context, index) {
+          return Container(
+            decoration: const BoxDecoration(
+                color: Color(0xFF000000),
+                border: Border(
+                    bottom: BorderSide(
+                        color: Color(0xFF272727),
+                        style: BorderStyle.solid,
+                        strokeAlign: BorderSide.strokeAlignInside))),
+            child: ListTile(
+              title: Text(tags[index],
+                  style: Theme.of(context).textTheme.bodyMedium),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+        label: const Text('New Tag'),
+        icon: const Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const AddTagScreen()),
+          );
+        },
       ),
     );
   }

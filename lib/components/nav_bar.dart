d48c913006e6a7e8a9edfbc7664def80bf4cfefc
Alina Kreimer <alina-kreimer@web.de>
2024-06-05 08:49:50 +0200

    add empty Setting Pages

--- lib/components/nav_bar.dart
+++ lib/components/nav_bar.dart
@@ -16,29 +16,34 @@ class NavBar extends StatelessWidget {
         padding: const EdgeInsets.all(0),
         elevation: 0.0,
         child: Container(
-          height: 60,
-          color: Color(int.parse('0xFF303030')),
+          height: 56,
+          color: Color(0xFF121212),
           child: Row(
             children: [
+
               navItem(
                 Icons.home_outlined,
                 pageIndex == 0,
                 'Home',
                 onTap: () => onTap(0),
               ),
+
               navItem(
                 Icons.sell_outlined,
                 pageIndex == 1,
                 'Tags',
                 onTap: () => onTap(1),
               ),
+
               const SizedBox(width: 80),
+              
               navItem(
                 Icons.analytics_outlined,
                 pageIndex == 3,
                 'Analytics',
                 onTap: () => onTap(3),
               ),
+
               navItem(
                 Icons.settings_outlined,
                 pageIndex == 4,

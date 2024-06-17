90c7becad87efdd8437bf22bfcdbc11711ae6ea1
golemt <tkosleck@gmail.com>
2024-06-17 16:45:11 +0200

    Line and PieChart are being generated

---
+++ lib/components/pie_chart.dart
@@ -0,0 +1,87 @@
+import 'package:finance_tracker/model/tag.dart';
+import 'package:finance_tracker/model/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
+import 'package:finance_tracker/file_controller.dart'; // Wird später für das laden von den richtigen Daten benötigt
+
+//TODO: Read the data from the file
+
+class MyChart extends StatefulWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      "Car","2024-03-13",[2],4500.00,["Arbeit"]
+    ),
+    Transaction(
+      "Baloons","2024-03-12",[0],-40.00,["Freizeit"]
+    ),
+    Transaction(
+      "Health Insurance","2024-04-01",[3],-150.00,["Versicherung"]
+    ),
+    Transaction(
+      "Online Course on Data Science","2024-04-15",[1],-200.00,["Bildung"]
+      ),
+  ];
+  final List<Tag> tags = [Tag("Freizeit", "Freizeitausgabe"), Tag("Bildung", "Bildungsausgabe"), Tag("Arbeit", "Arbeitsausgabe"), Tag("Versicherung", "Versicherungsausgabe")];
+  final ThemeData themeMode = ThemeData();
+
+  MyChart({Key? key});
+
+  @override
+  _MyChartState createState() => _MyChartState();
+}
+
+class _MyChartState extends State<MyChart> {
+  Map<String, double> dataMap = {};
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  void fetchData() {
+    Map<String, double> sumsByTag = {};
+
+    for (var tag in widget.tags) {
+      sumsByTag[tag.tagName] = 0;
+    }
+
+    for (var transaction in widget.transactions) {
+      if (transaction.transactionAmount < 0) {
+        for (var tag in transaction.transactionTag) {
+          if(tag < widget.tags.length) {
+            String tagName = widget.tags[tag].tagName;
+            if (sumsByTag.containsKey(tagName)) {
+            sumsByTag[tagName] = sumsByTag[tagName]! + transaction.transactionAmount;
+          }
+          }
+        }
+      }
+    }
+    sumsByTag.removeWhere((key, value) => value >= 0);
+
+    setState(() {
+      dataMap = sumsByTag;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return dataMap.isEmpty
+        ? const Text("No data available")
+        : PieChart(
+            dataMap: dataMap,
+            colorList: [
+              Colors.red.withOpacity(0.7),
+              Colors.green.withOpacity(0.7),
+              Colors.blue.withOpacity(0.7),
+            ],
+            chartValuesOptions: ChartValuesOptions(
+              showChartValuesInPercentage: true,
+              showChartValueBackground: false,
+              chartValueStyle: TextStyle(color: widget.themeMode.textTheme.bodyLarge?.color),
+            ),
+            chartType: ChartType.disc,
+          );
+  }
+}
--- lib/screens/analytics_screen.dart
+++ lib/screens/analytics_screen.dart
@@ -1,13 +1,27 @@
+import 'package:finance_tracker/components/linechart.dart';
 import 'package:flutter/material.dart';
+import 'package:finance_tracker/components/pie_chart.dart';
 
 class AnalyticsScreen extends StatelessWidget {
+
   const AnalyticsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text('Analytics Screen'),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300, // Feste Höhe für das Pie Chart
+              child: MyChart(),
+            ),
+            SizedBox(
+              height: 300, // Feste Höhe für das Line Chart
+              child: LineChartComponent(),
+            )
+          ]
+        )
       ),
     );
   }

78033111118b474445f333e7f82c2d90dc4d3807
golemt <tkosleck@gmail.com>
2024-06-18 17:50:08 +0200

    add tags + autogenerate tag list based on filecontroller

--- lib/screens/add_tag_screen.dart
+++ lib/screens/add_tag_screen.dart
@@ -1,17 +1,73 @@
+import 'package:finance_tracker/assets/color_palette.dart';
+import 'package:finance_tracker/file_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class AddTagScreen extends StatelessWidget {
+class AddTagScreen extends StatefulWidget {
   const AddTagScreen({super.key});
 
   @override
+  _AddTagScreenState createState() => _AddTagScreenState();
+}
+
+class _AddTagScreenState extends State<AddTagScreen> {
+  String tagName = "";
+  String tagDescription = "";
+  Color errorMessageColor = Colors.transparent;
+  String errorMessage = "";
+
+  @override
   Widget build(BuildContext context) {
+    final fileController = context.read<FileController>();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Add Tag', style: TextStyle(color: Theme.of(context).textTheme.bodyMedium?.color)),
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       ),
-      body: const Center(
-        child: Text('Add Tag Screen'),
+      body: Center(
+        child: Form(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Tag Name'),
+                  onChanged: (value) => tagName = value,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Tag Description'),
+                  onChanged: (value) => tagDescription = value,
+                ),
+                const SizedBox(height: 20),
+                TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.resolveWith((states) => NexusColor.accents),
+                  ),
+                  onPressed: () async {
+                    if (tagName.isEmpty) {
+                      setState(() {
+                        errorMessage = "Name cannot be empty";
+                        errorMessageColor = Colors.red;
+                      });
+                    } else if (fileController.listTag.any((tag) => tag.tagName == tagName)) {
+                      setState(() {
+                        errorMessage = "Tag already exists";
+                        errorMessageColor = Colors.red;
+                      });
+                    } else {
+                      await fileController.createTag(tagName, tagDescription);
+                      setState(() => errorMessageColor = Colors.transparent);
+                      Navigator.pop(context);
+                    }
+                  },
+                  child: Text('Add Tag', style: TextStyle(color: Theme.of(context).textTheme.bodyMedium?.color)),
+                ),
+                Text(errorMessage, style: TextStyle(color: errorMessageColor)),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/screens/tags_screen.dart
+++ lib/screens/tags_screen.dart
@@ -1,6 +1,8 @@
 import 'package:finance_tracker/assets/color_palette.dart';
+import 'package:finance_tracker/file_controller.dart';
 import 'package:finance_tracker/screens/add_tag_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class TagsScreen extends StatefulWidget {
   const TagsScreen({super.key});
@@ -10,121 +12,122 @@ class TagsScreen extends StatefulWidget {
 }
 
 class _TagScreenState extends State<TagsScreen> {
-
-  //TODO: use real data instead of static test data
-  final List<Map<String, String>> tags = [
-    {'title': 'Groceries', 'description': 'dsjflkhsdjkfhslkdfhkdshgkd'},
-    {'title': 'Utilities', 'description': 'djkshgflkghadkghfdjvnsdkn'},
-    {
-      'title': 'Entertainment',
-      'description':
-          'fdöybjndsökb iaugpnrehwgvrpvtiueiebrtbemhrawibghslegirwanötertwitb'
-    },
-  ];
+  @override
+  void initState() {
+    super.initState();
+    // Loading the tags initially when the screen is created
+    Future.microtask(() => context.read<FileController>().readTag());
+  }
 
   @override
   Widget build(BuildContext context) {
+    final fileController = context.watch<FileController>();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Tags'),
       ),
-      body: ListView.builder(
-        itemCount: tags.length,
-        itemBuilder: (context, index) {
-          final tag = tags[index];
-          return Container(
-            decoration: const BoxDecoration(
-                color: NexusColor.listBackground,
-                border: Border(
-                    bottom: BorderSide(
-                        color: NexusColor.text,
-                        style: BorderStyle.solid,
-                        strokeAlign: BorderSide.strokeAlignInside))),
-            child: ExpansionTile(
-              title: Text(tag['title']!,
-                  style: Theme.of(context).textTheme.bodyMedium),
-              children: <Widget>[
-                Container(
-                  color: NexusColor.background,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        TextField(
-                          decoration: InputDecoration(
-                            hintText: tag['title'],
-                            helperText: 'Name',
-                            filled: true,
-                          ),
-                        ), // Ende TextField 1
-                        const SizedBox(height: 8.0),
-                        TextField(
-                          decoration: InputDecoration(
-                            hintText: tag['description'],
-                            helperText: 'Description',
-                            filled: true,
-                          ),
-                        ), // Ende TextField 2
-                        const SizedBox(height: 16.0),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            ElevatedButton(
-                              onPressed: () {},
-                              style: ElevatedButton.styleFrom(
-                                shape: const CircleBorder(),
-                                padding: const EdgeInsets.all(8.0),
-                                minimumSize: const Size(37, 37),
-                                maximumSize: const Size(37, 37),
-                                backgroundColor: NexusColor.negative,
-                              ), // Ende ElevatedButton.styleFrom
-                              child: const Icon(
-                                Icons.delete,
-                                color: NexusColor.text,
-                                size: 20,
-                              ),
-                            ),
-                            const SizedBox(width: 240.0),
-                            // Ende ElevatedButton 1
-                            ElevatedButton(
-                              onPressed: () {},
-                              style: ElevatedButton.styleFrom(
-                                shape: const CircleBorder(),
-                                padding: const EdgeInsets.all(8.0),
-                                minimumSize: const Size(37, 37),
-                                maximumSize: const Size(37, 37),
-                                backgroundColor: NexusColor.positive,
-                              ), // Ende ElevatedButton.styleFrom
-                              child: const Icon(
-                                Icons.check,
-                                color: NexusColor.text,
-                                size: 20,
-                              ),
-                            ),
-                            // Ende ElevatedButton 2
-                          ], // Ende children Row
-                        ), // Ende Row
-                      ], // Ende children Column
-                    ), // Ende Column
+      body: fileController.listTag.isEmpty
+          ? Center(child: Text("No tags available", style: TextStyle(color: Theme.of(context).textTheme.bodyMedium?.color),))
+          : ListView.builder(
+              itemCount: fileController.listTag.length,
+              itemBuilder: (context, index) {
+                final tag = fileController.listTag[index];
+                return Container(
+                  decoration: const BoxDecoration(
+                      color: NexusColor.listBackground,
+                      border: Border(
+                          bottom: BorderSide(
+                              color: NexusColor.text,
+                              style: BorderStyle.solid,
+                              strokeAlign: BorderSide.strokeAlignInside))),
+                  child: ExpansionTile(
+                    title: Text(tag.tagName!,
+                        style: Theme.of(context).textTheme.bodyMedium),
+                    children: <Widget>[
+                      Container(
+                        color: NexusColor.background,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              TextField(
+                                decoration: InputDecoration(
+                                  hintText: tag.tagName,
+                                  helperText: 'Name',
+                                  filled: true,
+                                ),
+                              ), // Ende TextField 1
+                              const SizedBox(height: 8.0),
+                              TextField(
+                                decoration: InputDecoration(
+                                  hintText: tag.tagDescription,
+                                  helperText: 'Description',
+                                  filled: true,
+                                ),
+                              ), // Ende TextField 2
+                              const SizedBox(height: 16.0),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      fileController.resetTag();
+                                    },
+                                    style: ElevatedButton.styleFrom(
+                                      shape: const CircleBorder(),
+                                      padding: const EdgeInsets.all(8.0),
+                                      minimumSize: const Size(37, 37),
+                                      maximumSize: const Size(37, 37),
+                                      backgroundColor: NexusColor.negative,
+                                    ), // Ende ElevatedButton.styleFrom
+                                    child: const Icon(
+                                      Icons.delete,
+                                      color: NexusColor.text,
+                                      size: 20,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 240.0),
+                                  // Ende ElevatedButton 1
+                                  ElevatedButton(
+                                    onPressed: () {},
+                                    style: ElevatedButton.styleFrom(
+                                      shape: const CircleBorder(),
+                                      padding: const EdgeInsets.all(8.0),
+                                      minimumSize: const Size(37, 37),
+                                      maximumSize: const Size(37, 37),
+                                      backgroundColor: NexusColor.positive,
+                                    ), // Ende ElevatedButton.styleFrom
+                                    child: const Icon(
+                                      Icons.check,
+                                      color: NexusColor.text,
+                                      size: 20,
+                                    ),
+                                  ),
+                                  // Ende ElevatedButton 2
+                                ], // Ende children Row
+                              ), // Ende Row
+                            ], // Ende children Column
+                          ), // Ende Column
+                        ),
+                      ), // Ende Padding
+                    ],
                   ),
-                ), // Ende Padding
-              ],
+                );
+              },
             ),
-          );
-        },
-      ),
       floatingActionButton: FloatingActionButton.extended(
-        shape:
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
         label: const Text('New Tag'),
         icon: const Icon(Icons.add),
         backgroundColor: NexusColor.accents,
-        onPressed: () {
-          Navigator.push(
+        onPressed: () async {
+          await Navigator.push(
             context,
             MaterialPageRoute(builder: (context) => const AddTagScreen()),
           );
+          fileController.readTag(); // Update tags after returning from AddTagScreen
         },
       ),
     );

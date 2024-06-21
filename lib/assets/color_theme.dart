80c981b12a977c2846bd8e0190ffbd58dd483351
Jasper-Finn Schröder <jasper-finn.schroeder@deutschebahn.com>
2024-06-21 23:07:18 +0200

    update: rebase

--- lib/assets/color_theme.dart
+++ lib/assets/color_theme.dart
@@ -4,6 +4,7 @@ import 'package:finance_tracker/assets/color_palette.dart';
 class NexusTheme {
   static ThemeData get nexusTheme {
     return ThemeData(
+      useMaterial3: true,
       scaffoldBackgroundColor: NexusColor.background,
       primaryColor: NexusColor.secondary,
 
@@ -29,11 +30,33 @@ class NexusTheme {
           borderSide: BorderSide(color: NexusColor.divider),
         ),
         labelStyle: TextStyle(color: NexusColor.text),
+        filled: true,
         helperStyle: TextStyle(color: NexusColor.subText),
         hintStyle: TextStyle(color: NexusColor.text),
+        border: OutlineInputBorder(),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: NexusColor.accents),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: NexusColor.divider),
+        ),
+        labelStyle: TextStyle(color: NexusColor.text),
       ),
       floatingActionButtonTheme: const FloatingActionButtonThemeData(
         foregroundColor: NexusColor.white,
+      ),
+      appBarTheme: const AppBarTheme(
+        backgroundColor: NexusColor.navigation,
+        elevation: 10.0,
+        surfaceTintColor: Colors.transparent,
+        iconTheme: IconThemeData(
+          color: Colors.white,
+        ),
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+        ),
       ),
     );
   }
